82fe4e29da8f88f4e06f39ba942b72c47be78fa8
koho <ihopeme.hd@gmail.com>
2017-05-21 11:25:58 +0800

    Create LitterStar2.rb

---
+++ LitterStar2.rb
@@ -0,0 +1,11 @@
+# Little Star
+loop do
+  play_pattern_timed [60, 60, 67, 67, 69, 69, 67], [0.5]
+  sleep 0.5
+  play_pattern_timed [65, 65, 64, 64, 62, 62, 60], [0.5]
+  sleep 0.5
+  play_pattern_timed [67, 67, 65, 65, 64, 64, 62], [0.5]
+  sleep 0.5
+  play_pattern_timed [67, 67, 65, 65, 64, 64, 62], [0.5]
+  sleep 0.5
+end
